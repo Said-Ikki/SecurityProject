3dbd16cd699629482b7b772732be7f459ec2029c
Said Ikki <ssikki@lakeheadu.ca>
2024-11-09 21:21:30 -0500

    added all the files needed to make the login work - main.py - privatekey.pem - public.pem - publickey.pem Note: there is a variable named "driver" in main. Change that to whatever USB drive that happens on your computer

--- main.py
+++ main.py
@@ -6,67 +6,80 @@ import msvcrt
 import os
 import sys
 import rsa
+import json
 
 import cryptography
 from cryptography.fernet import Fernet
 from getpass import getpass
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 import pyautogui
 
 import requests
 import socket
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-    print(Fernet.generate_key())
-    key = b'5i5davgtM7BIRKNfxRXzaIt6AvFRajleCV7gDpEz11Q='
-    fernet = Fernet(key)
+    #    private_key = RSA.generate(4096)
+    #   public_key = private_key.publickey()
+    #  f = open('privatekey.pem', 'wb')
+    # f.write(private_key.exportKey('PEM'))
+    # f.close()
+    #    f = open('publickey.pem', 'wb')
+    #   f.write(public_key.exportKey('PEM'))
+    #  f.close()
+
+    private_key = RSA.importKey(open('privatekey.pem').read())
+    public_key = RSA.importKey(open('publickey.pem').read())
+
+    cipher = PKCS1_OAEP.new(public_key)
 
     create_new_account_button_name = 'Create a New Account'
     login_button_name = 'Login'
 
     isLoginOrNewAccount = pyautogui.confirm('Would you like to create a new account or log into a previous account??',
-                          buttons=[create_new_account_button_name, login_button_name])
+                                            buttons=[create_new_account_button_name, login_button_name])
     print(isLoginOrNewAccount)
+    drive = "F"
 
     if isLoginOrNewAccount == create_new_account_button_name:
         new_username = pyautogui.password('Enter username: ')
         new_password = pyautogui.password('Enter password: ')
 
-        encUsername = fernet.encrypt(new_username.encode())
-        encPassword = fernet.encrypt(new_password.encode())
+        encUsername = cipher.encrypt(new_username.encode())
+        encPassword = cipher.encrypt(new_password.encode())
 
         try:
-            os.mkdir("F:\\Top Secret Artifact\\Login Files")
+            os.mkdir(drive + ":\\Top Secret Artifact\\Login Files")
         except FileExistsError:
             pass
 
-        f = open( str("F:\\Top Secret Artifact\\Login Files\\user_info.txt") , "w")
-        f.write( str(encUsername.decode()) )
-        f.write( "\n" + str(encPassword.decode()  ) )
+        f = open(str(drive + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt"), "wb")
+        f.write(encUsername)
+        f.close()
+        f = open(str(drive + ":\\Top Secret Artifact\\Login Files\\user_info_2.txt"), "wb")
+        f.write(encPassword)
         f.close()
 
     if isLoginOrNewAccount == login_button_name:
         entered_username = pyautogui.password('Enter username: ')
         entered_password = pyautogui.password('Enter password: ')
 
-        # open the sample file used
-        #file = open('user_info.txt')
         try:
-            file = open("F:\\Top Secret Artifact\\Login Files\\user_info.txt")
-            # read the content of the file opened
-            content = file.readlines()
+            file = open(drive + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt", "rb")
+            content1 = file.read()
+            file.close()
 
-            #print("unencoded: " + content[0])
-            actual_username = fernet.decrypt(content[0]).decode()
-            actual_password = fernet.decrypt(content[1]).decode()
+            file = open(drive + ":\\Top Secret Artifact\\Login Files\\user_info_2.txt", "rb")
+            content2 = file.read()
+            file.close()
+
+            cipher = PKCS1_OAEP.new(private_key)
+
+            actual_username = cipher.decrypt(content1).decode()
+            actual_password = cipher.decrypt(content2).decode()
 
             isUsernameMatch = (actual_username == entered_username)
             isPasswordMatch = (actual_password == entered_password)
@@ -76,60 +89,16 @@ if __name__ == '__main__':
                 print('Login successful!')
                 hostname = socket.gethostname()
                 ip_address = socket.gethostbyname(hostname)
-                #sending_key = b'8F1F2787abOWcBkoS3CjPcpj-j9BJsUmITdP0lqopos='
-                #sendingFernet = Fernet(sending_key)
-                #encryptedIP = sendingFernet.encrypt(ip_address.encode())
-                #print(requests.post(url="http://192.168.250.63:5000/validate", data=encryptedIP.decode()))
-                print(requests.post(url="http://192.168.250.63:5000/validate", data=ip_address))
+
+                public_sender = RSA.importKey(open('public.pem').read())
+                cipher = PKCS1_OAEP.new(public_sender)
+                msg = cipher.encrypt(ip_address.encode())
+
+                print("...Checking Status...")
+                print(requests.post(url="http://192.168.250.63:5000/validate", data=msg).json)
             else:
                 print('Login failed')
             print("-------------------")
         except FileNotFoundError:
             print('Please Insert Your Ultra-Secret Super-Duper Mega-USB before proceeding')
 
-        #print(actual_username, actual_password)
-
-
-    message = "hello there"
-    encMessage = fernet.encrypt(message.encode())
-    decMessage = fernet.decrypt(encMessage).decode()
-    print(message)
-    print(encMessage)
-    print(decMessage)
-
-    print( os.path.isfile("C:\\Windows\\System32\\notepad.exe") )
-    print( os.path.isfile( "F:\\Top Secret Artifact\\optimal.png" ) )
-
-    f = open("demofile3.txt", "w")
-    f.write("Woops! I have deleted the content!")
-    f.close()
-
-    # open and read the file after the overwriting:
-    f = open("demofile3.txt", "r")
-    print(f.read())
-
-    #key = input("Enter the Password")
-    key = pyautogui.password('Enter password: ')
-    if key != "key":
-        print("Wrong Password")
-        sys.exit()
-
-    drives = {"C", "D", "E", "F", "G"}
-
-    hasFile = False
-    for drive in drives:
-        isMainDrive   = os.path.isfile(drive + ":\\Windows\\System32\\notepad.exe"   )
-        isUSBwithFile = os.path.isfile(drive + ":\\Top Secret Artifact\\optimal.png" )
-        if isMainDrive == False and isUSBwithFile == True:
-            print("Access Granted: Drive " + drive)
-            hasFile = True
-        #elif isMainDrive:
-        #    print("Drive " + drive + " is a main drive")
-        #elif isUSBwithFile == False:
-        #    print("Drive " + drive + " does not contain the file")
-    if not hasFile:
-        print("Access Denied: You Forgot Your External Device")
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
