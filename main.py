5a3ee3dbea8e7920ab5b7cc5ec406ad0334354b1
Said Ikki <ssikki@lakeheadu.ca>
2024-11-16 15:35:51 -0500

    cleaned up the code - added functions - made it more 'flexible' for different drives - added comments also added html pages for pretty presentation - they are made by ChatGPT tho so keep that in mind - I'd prefer handmade HTML pages but I don't want to do it myself lol

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-import msvcrt
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import os
 import sys
 import rsa
@@ -19,86 +14,154 @@ import pyautogui
 import requests
 import socket
 
+drive = "F" # default driver for testing
+drives = ["D", "E", "F", "G"] # potential USB drives
+def create_keys(): # creates the private and public keys
+    private_key = RSA.generate(4096) # create private
+    public_key = private_key.publickey() # create public based on private
+    f = open('privatekey.pem', 'wb') # save private key
+    f.write(private_key.exportKey('PEM'))
+    f.close()
+    f = open('publickey.pem', 'wb') # save public key
+    f.write(public_key.exportKey('PEM'))
+    f.close()
+
+def write_user_info(driver, encUsername, encPassword): # write encrypted unser info to files
+    try:
+        os.mkdir(driver + ":\\Top Secret Artifact\\Login Files") # check if this file is ok to write to
+        f = open(str(driver + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt"), "wb") #write the username to file 1
+        f.write(encUsername)
+        f.close()
+        f = open(str(driver + ":\\Top Secret Artifact\\Login Files\\user_info_2.txt"), "wb") # write the password to file 2
+        f.write(encPassword)
+        f.close()
+        return True
+    except FileNotFoundError: # if driver is bad
+        return False # say no op was performed
+    except FileExistsError: # if a user info already exists
+        print("Info Already Exists! Rewriting") # rewrite it
+        f = open(str(driver + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt"), "wb")
+        f.write(encUsername)
+        f.close()
+        f = open(str(driver + ":\\Top Secret Artifact\\Login Files\\user_info_2.txt"), "wb")
+        f.write(encPassword)
+        f.close()
+        return True
+
+def read_user_info(): # get user info from files
+    isDriveWork = False # flag for figuring out if a USB is inserted
+    for d in drives: # check all predifined drives
+        if isDriveWork: # if one is already found
+            break # end
+        # check if file is there
+        isDriveWork = os.path.isfile(d + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt")
+        if isDriveWork: # if it is
+            driver = d # thats the drive we take info from
+            break # end loop
+    if not isDriveWork: # if no drives work
+        raise FileNotFoundError # no USB is inserted
+
+    # read information from file
+    file = open(driver + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt", "rb")
+    content1 = file.read()
+    file.close()
+
+    file = open(driver + ":\\Top Secret Artifact\\Login Files\\user_info_2.txt", "rb")
+    content2 = file.read()
+    file.close()
+    return content1, content2
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    #    private_key = RSA.generate(4096)
-    #   public_key = private_key.publickey()
-    #  f = open('privatekey.pem', 'wb')
-    # f.write(private_key.exportKey('PEM'))
-    # f.close()
-    #    f = open('publickey.pem', 'wb')
-    #   f.write(public_key.exportKey('PEM'))
-    #  f.close()
-
+    # read keys in folder
     private_key = RSA.importKey(open('privatekey.pem').read())
     public_key = RSA.importKey(open('publickey.pem').read())
 
-    cipher = PKCS1_OAEP.new(public_key)
+    cipher = PKCS1_OAEP.new(public_key) # create encryptor
 
+    # variables for button names
+    # when the buttons are chosen, the string is returned so it makes for easy comparisons
     create_new_account_button_name = 'Create a New Account'
     login_button_name = 'Login'
 
+    # will the user login or register?
     isLoginOrNewAccount = pyautogui.confirm('Would you like to create a new account or log into a previous account??',
                                             buttons=[create_new_account_button_name, login_button_name])
     print(isLoginOrNewAccount)
-    drive = "F"
 
-    if isLoginOrNewAccount == create_new_account_button_name:
-        new_username = pyautogui.password('Enter username: ')
-        new_password = pyautogui.password('Enter password: ')
 
-        encUsername = cipher.encrypt(new_username.encode())
+    if isLoginOrNewAccount == create_new_account_button_name: # if register
+        new_username = pyautogui.password('Enter username: ') # get username
+        new_password = pyautogui.password('Enter password: ') # and password
+        # hidden ofc
+
+        encUsername = cipher.encrypt(new_username.encode()) # encrypt both
         encPassword = cipher.encrypt(new_password.encode())
 
+        # save it somewhere
+        for d in drives: # save it in all drives
+            didWrite = write_user_info(d, encUsername, encPassword)
+        if didWrite == False: # if it wasn't able to save, it was because no USB was inserted
+            print("Failed to save information. Please Insert Your Ultra-Secret Super-Duper Mega-USB before proceeding ")
+        elif didWrite == True: # otherwise, its good
+            print("Successfully Saved Information")
+
+        #try:
+         #   os.mkdir(drive + ":\\Top Secret Artifact\\Login Files")
+        #except FileExistsError:
+         #   pass
+
+        #f = open(str(drive + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt"), "wb")
+        #f.write(encUsername)
+        #f.close()
+        #f = open(str(drive + ":\\Top Secret Artifact\\Login Files\\user_info_2.txt"), "wb")
+        #f.write(encPassword)
+        #f.close()
+
+    if isLoginOrNewAccount == login_button_name: # if they want to login
+        entered_username = pyautogui.password('Enter username: ') # get username
+        entered_password = pyautogui.password('Enter password: ') # and password
+        #  hidden ofc
+
         try:
-            os.mkdir(drive + ":\\Top Secret Artifact\\Login Files")
-        except FileExistsError:
-            pass
+            #file = open(drive + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt", "rb")
+            #content1 = file.read()
+            #file.close()
 
-        f = open(str(drive + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt"), "wb")
-        f.write(encUsername)
-        f.close()
-        f = open(str(drive + ":\\Top Secret Artifact\\Login Files\\user_info_2.txt"), "wb")
-        f.write(encPassword)
-        f.close()
+            #file = open(drive + ":\\Top Secret Artifact\\Login Files\\user_info_2.txt", "rb")
+            #content2 = file.read()
+            #file.close()
 
-    if isLoginOrNewAccount == login_button_name:
-        entered_username = pyautogui.password('Enter username: ')
-        entered_password = pyautogui.password('Enter password: ')
+            content1, content2 = read_user_info() # get info
 
-        try:
-            file = open(drive + ":\\Top Secret Artifact\\Login Files\\user_info_1.txt", "rb")
-            content1 = file.read()
-            file.close()
+            cipher = PKCS1_OAEP.new(private_key) # get decryptor
 
-            file = open(drive + ":\\Top Secret Artifact\\Login Files\\user_info_2.txt", "rb")
-            content2 = file.read()
-            file.close()
+            actual_username = cipher.decrypt(content1).decode() # decrypt and decode
+            actual_password = cipher.decrypt(content2).decode() # to process it later
 
-            cipher = PKCS1_OAEP.new(private_key)
-
-            actual_username = cipher.decrypt(content1).decode()
-            actual_password = cipher.decrypt(content2).decode()
-
-            isUsernameMatch = (actual_username == entered_username)
-            isPasswordMatch = (actual_password == entered_password)
+            isUsernameMatch = (actual_username == entered_username) # does the input
+            isPasswordMatch = (actual_password == entered_password) # match with whats saved
 
             print("-------------------")
-            if isUsernameMatch and isPasswordMatch:
+            if isUsernameMatch and isPasswordMatch: # if it is
                 print('Login successful!')
-                hostname = socket.gethostname()
-                ip_address = socket.gethostbyname(hostname)
+                hostname = socket.gethostname() # get hostname
+                ip_address = socket.gethostbyname(hostname) # to get IP
 
-                public_sender = RSA.importKey(open('public.pem').read())
-                cipher = PKCS1_OAEP.new(public_sender)
-                msg = cipher.encrypt(ip_address.encode())
+                public_sender = RSA.importKey(open('public.pem').read()) # use different encryptor provided by server
+                cipher = PKCS1_OAEP.new(public_sender) # get server encryptor
+                msg = cipher.encrypt(ip_address.encode()) # encrypt IP address as msg
+                # this github has all files squished together
+                # ideally this code only has access to the public key for the server
+                # so it can only encrypt, reducing vulnerability
 
-                print("...Checking Status...")
-                print(requests.post(url="http://192.168.250.63:5000/validate", data=msg).json)
-            else:
-                print('Login failed')
+                print("...Checking Status...") # send msg
+                print(requests.post(url="http://192.168.250.68:5000/validate", data=msg).json)
+            else: # if the info doesnt match
+                print('Login failed') # login failed
             print("-------------------")
-        except FileNotFoundError:
+        except FileNotFoundError: # only if USB not entered
             print('Please Insert Your Ultra-Secret Super-Duper Mega-USB before proceeding')
 
--- server.py
+++ server.py
@@ -4,7 +4,8 @@ from cryptography.fernet import Fernet
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-private_key = RSA.importKey(open('private.pem').read())
+private_key = RSA.importKey(open('private.pem').read()) # get decryption key
+# code for creating keys if we need to
 #public = private_key.publickey()
 #private_key = RSA.generate(2048)
 #f = open('public.pem', 'wb')
@@ -15,23 +16,32 @@ app = Flask(__name__)
 
 approved_ip_addresses = []
 
-@app.route("/validate", methods=['GET', 'POST'])
+@app.route("/validate", methods=['GET', 'POST']) # other code sends IP here to get access
 def hello_world():
     #print("encrypted in: ", request.data.decode())
     #actual_ip = fernet.decrypt(request.data.decode()).decode()
     #print("decrypted: ", actual_ip)
-    ip = request.data
-    cipher = PKCS1_OAEP.new(private_key)
-    actual_ip = cipher.decrypt(ip).decode()
-    approved_ip_addresses.append(actual_ip)
+    ip = request.data # extracts msg
+    cipher = PKCS1_OAEP.new(private_key) # sets up decryptor
+    actual_ip = cipher.decrypt(ip).decode() # gets original msg
+    approved_ip_addresses.append(actual_ip) # adds IP to allowed IPs
     return actual_ip
 
 @app.route("/", methods=['GET', 'POST'])
 def thing():
-    if request.remote_addr not in approved_ip_addresses:
-        return "no approved IP"
+    if request.remote_addr not in approved_ip_addresses: # if this comes from an unvalidated UP
+        #return "no approved IP" # show them one screen
+        return  render_template("aceess_denied.html")
     else:
-        return "whats up bro?"
+        #return "whats up bro?" # show them a different screen
+        return render_template("aceess_granted.html")
 
 #if __name__ == "__main__":
 app.run(host="0.0.0.0", port=5000)
+
+### DISCLOSURE ###
+# THE HTML PAGES ARE CREATED BY CHATGPT
+# they are intended to be a stand-in for what the system *could* achieve
+# everything else is hand-coded by Said, yours truly
+# to see results without the html pages, there is a commented line above each 'return' stmt
+# uncomment them
